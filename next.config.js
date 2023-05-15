9e835cbd10d288e49e9fdcd1722b67c8de56fd27
Mertcan Polat <97837998+Mertcan-polat@users.noreply.github.com>
2023-05-15 22:02:50 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  */
-const nextConfig = {
-  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  reactStrictMode: true,
+
+module.exports = module.exports = {
+  // Diğer yapılandırma seçenekleri burada yer alabilir
+  output: {
+    // output seçeneğini "export" olarak ayarla
+    export: {},
+  },
   i18n: {
     locales: ["tr", "en", "de", "nl", "it"],
     defaultLocale: "tr",
   },
 };
-
-module.exports = nextConfig;
